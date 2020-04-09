8c3c8357ea0a8755786a7a4275a753fe75f7bb21
ahmedkhaled023 <ahmedkhaled023@gmail.com>
2020-04-09 16:11:49 +0200

    Zabat el scene ya H

--- scripts/V2T.py
+++ scripts/V2T.py
@@ -108,12 +108,12 @@ def start_vrep_node(mode = 'p'):
 				vrep_param.append(v.get_torque(i))
 			for i in range(12):
 				vrep_param.append(v.get_vel(i))
-			linear_accs=v.imu_read()
-			anglular_accs=v.gyro_read()
-			for linear_acc in linear_accs:
-				vrep_param.append(linear_acc)
-			for angular_acc in anglular_accs :
-				vrep_param.append(angular_acc)
+			#linear_accs=v.imu_read()
+			#anglular_accs=v.gyro_read()
+			#for linear_acc in linear_accs:
+			#	vrep_param.append(linear_acc)
+			#for angular_acc in anglular_accs :
+			#	vrep_param.append(angular_acc)
 			
 
 
