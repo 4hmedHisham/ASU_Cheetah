de508ff6d1a1838061017eeaf68a3c63bf61a85c
Ahmed Hisham <4hmed.hisham@gmail.com>
2020-04-10 12:42:10 +0200

    Adjusted V2T and URDF

--- scripts/V2T.py
+++ scripts/V2T.py
@@ -4,17 +4,22 @@ import time
 import numpy as np
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 import GP2_Vrep_V3 as v
+boolean = 1
+mode = 'p'
 #ADDED VREP Iinit in one function
 counter=0
 testing=False
 printing=False
+'''
 if not testing :
 	mode=raw_input("Please enter 'p' for position control or 't' for torque control:  ")
 	while ((mode!='p') and (mode!='t')):
 		print('Please enter a vaild control mode')
 		mode=raw_input("Please enter 'p' for position control or 't' for torque control:  ")
 	v.vrep_init(19997,mode)
+'''
 def is_number(n):
     try:
         float(n)   # Type-casting the string to `float`.
@@ -62,14 +67,36 @@ def set_vrep_angels(data):
 		print('Recieved ')
 	# print(ang)
 def start_vrep_node():
+	global mode
 	''' This function instialze the node responsible for vrep/ros interaction.'''
 	pub = rospy.Publisher('getter', Float32MultiArray, queue_size=10)
 	sub = rospy.Subscriber('setter',String,set_vrep_angels)
+	sub = rospy.Subscriber('disable',Bool,disable)
 	rospy.Subscriber('torques',String,set_vrep_torques)	
 	rospy.init_node('vrep', anonymous=True)
 	print("ROS NODE INTIALIZED")
 	rate = rospy.Rate(100) # 10hz
+	v.vrep_init(19997)
+	
+	counterrr = 0
+	init=0
 	while not rospy.is_shutdown():
+		if init==0:
+			#raw_input("INIT UR NODES")
+			init=init+1
+		
+		if counterrr == 1:
+				mode=raw_input("Please enter 'p' for position control or 't' for torque control:  ")
+				while ((mode!='p') and (mode!='t')):
+					print('Please enter a vaild control mode')
+					mode=raw_input("Please enter 'p' for position control or 't' for torque control:  ")
+				# if mode == 't':
+				# 	raw_input("Press enter any key to disable control loop: ")
+    			# 	v.ctrl_en(mode)
+		
+				
+				
+
 		i=0
 		total = Float32MultiArray()
 		total.data = []
@@ -104,7 +131,8 @@ def start_vrep_node():
 		total.data=vrep_param
 		#print(total.data)
 		pub.publish(total)
-
+		counterrr = counterrr +1 
+		#print(counterrr,"ahoo")
 		rate.sleep()
 
 def set_vrep_torques(data):
@@ -145,7 +173,21 @@ def set_vrep_torques(data):
 	if printing == True:
 		print('Recieved ')
 	# print(ang)
+def disable(data):
+	en=data.data
+	print("DATA IS ")
+	print(en)
+	if(mode=='p'):
+		print("You are in Position Control mode, hence u dont have access to chance control loop")
+	elif(mode=='t'):
+		if en==False:
+			v.ctrl_en('p')
+			print("Control loop enabled!")
+		else :
+			v.ctrl_en('t')
+			print("Control loop disabled!")
 
+#SO GIT YA5OD BALO
 if __name__ == '__main__':
 	try:
 		
