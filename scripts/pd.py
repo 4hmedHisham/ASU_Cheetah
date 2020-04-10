12e56bb1ec3a09c303ebd347d190f22653623f0e
Omar Islam <omarislam97@gmail.com>
2020-04-10 05:20:05 +0200

    ..

--- scripts/pd.py
+++ scripts/pd.py
@@ -3,27 +3,29 @@ import numpy as np
 from numpy import sin , cos , arctan2
 from timeit import default_timer as time
 import rospy 
-from std_msgs.msg import Float32MultiArray , String
+from std_msgs.msg import Float32MultiArray , String, Bool
 
 start = time()
 rospy.init_node('Impedance',anonymous=True)
 pub= rospy.Publisher('torques',String,queue_size=10)
+pub1 = rospy.Publisher('disbale',Bool,queue_size=10)
 rate = rospy.Rate(1000)
 
 l1 = 244.59
 l2 = 208.4
 a = 20
-kp = 8.0
-kd = 0.25
+kp = 0.5
+kd = 0.01
 sizeof_fwdk_array = 24 # x1 y1 z1 x2 y2 z2....
 sizeof_desired_array = 3
 sizeof_getter_array = 42
 leg_no = 1
 current_time = 0
-x_current = 9
-y_current = 9
-x_desired = 10
-y_desired = 10
+prev_time = 0
+x_current = 0
+y_current = 0
+x_desired = -390
+y_desired = 0
 prev_r = 0
 prev_theta = 0
 theta_knee = 0
@@ -34,12 +36,14 @@ def polar_jacoian(theta3):
     theta_row = [[1, (l2 * l2 + l1 * l2 * cos(theta3) / (l1 * l1 + l2 * l2 + 2 * l1 * l2 * cos(theta3)))]]
     Jp = np.concatenate((r_row, theta_row), axis=0)
     Jp = Jp.reshape(2,2)
+    #print(theta3)
     return np.transpose(Jp)
 
 
 def cart2polar(x,y):
     r = np.sqrt(x**2 + y**2)
     theta = arctan2(y, x)
+    theta = theta * (180.0/np.pi)
     return r, theta
 
 
@@ -67,14 +71,13 @@ def current_theta(data):
     msg = data.data
     msg = np.reshape(msg,sizeof_getter_array)
     global theta_knee
-    theta_knee = msg[(leg_no*3)-1]
+    theta_knee = msg[(leg_no*3)-1] * (180.0/np.pi)
 
 
 def pd():
     global current_time,prev_time
     global x_desired, y_desired,x_current,y_current
     global prev_r, prev_theta
-    prev_time = current_time
     current_time = time()
     elapsed_time = current_time - prev_time
     error_x = x_desired - x_current #5
@@ -82,9 +85,14 @@ def pd():
     r,theta = cart2polar(error_x,error_y)
     p_error = kp*(r+theta)
     d_error = kd*((r - prev_r)/elapsed_time) + kd*((theta - prev_theta)/elapsed_time)
+    print('r',r,prev_r,'-------','theta',theta,prev_theta)
+    print(error_x,error_y)
     prev_r = r
     prev_theta = theta
+    prev_time = current_time
     output = [[p_error,d_error]]
+    #print(output)
+    
     return output 
     
     
@@ -98,17 +106,21 @@ def listener_theta():
     rospy.Subscriber('getter',Float32MultiArray,current_theta)
 listener_current_pos()
 listener_desired_pos()
+listener_theta()
+pub1.publish(1)
 print("Subcribed")
 while not rospy.is_shutdown():
-    
-    listener_theta()
     torques = np.matmul(pd(),polar_jacoian(theta_knee))
     torques = torques.flatten()
     t = "%s %s"%((3*leg_no)-2 , torques[0])
+    #print(t)
     pub.publish(t)
     t = "%s %s"%((3*leg_no)-1  , torques[1])
     pub.publish(t)
-    rate.sleep()
+    #print(np.matmul(pd(),polar_jacoian(theta_knee)))
+    #print(start)
+   #print(x_current,y_current)
+    
     
 
 
